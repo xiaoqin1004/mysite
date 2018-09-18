3805cc4adc6e0a1a4b6ab6eda67951a6733fe456
xiaoqin1004 <42384768+xiaoqin1004@users.noreply.github.com>
2018-09-18 23:56:49 +0800

    路由配置写法修改为Django2.1版本写法path

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls', namespace='polls')),
-    url(r'^admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from django.urls.converters import SlugConverter
 from django.urls import register_converter
-from django.conf.urls import url
+
 from .import views
 # register_converter(SlugConverter, 'question_id')
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
